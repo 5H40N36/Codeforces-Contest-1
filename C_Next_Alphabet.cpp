b6638d40427931389bf0545a98c07a7815460752
5H40N <shaonshah36@gmail.com>
2023-10-29 02:08:20 +0600

    Add files via upload

---
+++ C_Next_Alphabet.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char a;
+    cin>>a;
+    if(a=='z') cout<<(char)('z'-25)<<endl;
+    else cout<<(char)(a+1)<<endl;
+    return 0;
+}
